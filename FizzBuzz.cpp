a33d8939190c04a9b869e839e25885eedaf8b3df
jwongv <jwongqw@gmail.com>
2014-06-18 20:16:55 -0700

    Update FizzBuzz.cpp

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,6 +1,11 @@
 //Jacky Wong
 //FizzBuzz Test Code
 
+//This file contains the code for the FizzBuzz test.
+//Prints out numbers 1 to 100. Numbers divisible by 3 are replaced by Fizz.
+//Numbers divisible by 5 are replaced by Buzz. 
+//Numbers divisible by 3 and 5 are replaced by FizzBuzz.
+
 #include <iostream>
 #include <cstdlib>
 using namespace std;
